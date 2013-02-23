96039045e1a77d02c90238240c8e3c6a333daa91
Tiago Teixeira <tiagocmtex@gmail.com>
2013-02-23 23:01:45 +0000

    Criada a aplicação

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orca::Application.config.secret_token = 'b027646d3eee75121fa04e76257ea48f267df4abd2680e376c45892f311a785303cb5676c502ba04aef70b02779d83a035715214b1921a279280576c9ea2d720'
