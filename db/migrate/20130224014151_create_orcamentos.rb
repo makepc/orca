ff1f87d3f265bb733872107daea87297758576ff
Tiago Teixeira <tiagocmtex@gmail.com>
2013-02-24 01:52:16 +0000

    Alterado o ficheiro de migração da BD

--- db/migrate/20130224014151_create_orcamentos.rb
+++ db/migrate/20130224014151_create_orcamentos.rb
@@ -1,12 +1,12 @@
 class CreateOrcamentos < ActiveRecord::Migration
   def change
     create_table :orcamentos do |t|
-      t.string :local
+      t.string :local, :null => false
       t.text :descricao
-      t.decimal :distancia
-      t.string :cli_nome
+      t.decimal :distancia, :null => false
+      t.string :cli_nome, :null => false
       t.text :cli_morada
-      t.decimal :cli_contacto
+      t.decimal :cli_contacto, :limit => 10, :null => false
 
       t.timestamps
     end
