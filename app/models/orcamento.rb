9db9e430382aa605d40bb980d34e64b1324cd222
Tiago Teixeira <tiagocmtex@gmail.com>
2013-02-26 23:35:29 +0000

    Adicionado validatores para campos de Orçamento e removido CSS não usado

--- app/models/orcamento.rb
+++ app/models/orcamento.rb
@@ -1,3 +1,4 @@
 class Orcamento < ActiveRecord::Base
   attr_accessible :cli_contacto, :cli_morada, :cli_nome, :descricao, :distancia, :local
+  validates_presence_of :cli_contacto, :cli_morada, :cli_nome, :descricao, :distancia, :local
 end
